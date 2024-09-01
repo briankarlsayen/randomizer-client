53c4493ae3670e59807b0f59d4861c21b82ddc33
blu3fire89 <blu3fire89@gmail.com>
2024-09-01 19:25:09 +0800

    update api

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 const timeoutMs = 5000;
 
 export const instance = axios.create({
-  baseURL: "http://localhost:5250",
+  // baseURL: "http://localhost:5250",
+  baseURL: "http://192.168.1.89/api",
   timeout: timeoutMs,
   // signal: abortSignal(timeoutMs),
 });
